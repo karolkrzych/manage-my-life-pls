8b7372c3e6edbc748e8368a570053ac4ebc93a9d
karolkrzych <karolkrzych@gmail.com>
2021-05-30 15:29:30 +0200

    connecting to firebase, optimistic updates

--- src/app/components/kanban/kanban.component.ts
+++ src/app/components/kanban/kanban.component.ts
@@ -4,40 +4,56 @@ import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
 import { TaskDialogResult } from './task-dialog/task-dialog';
 import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+const getObservable = (collection: AngularFirestoreCollection<Task>) => {
+  const subject = new BehaviorSubject<Task[]>([]);
+  collection.valueChanges({ idField: 'id' }).subscribe((val: Task[]) => {
+    subject.next(val);
+  });
+  return subject;
+}
 @Component({
   selector: 'app-kanban',
   templateUrl: './kanban.component.html',
   styleUrls: ['./kanban.component.scss'],
 })
 export class KanbanComponent {
-  todo: Task[] = [
-    {
-      title: 'Grab milk',
-      description: 'go and get that milk boi',
-    },
-    {
-      title: 'Do stuff',
-      description: 'i wgl',
-    },
-  ];
-  inProgress: Task[] = [];
-  done: Task[] = [];
 
-  constructor(private dialog: MatDialog) {}
+  todo = getObservable(this.store.collection('todo')) as Observable<Task[]>;
+  inProgress = getObservable(this.store.collection('inProgress')) as Observable<Task[]>;
+  done = getObservable(this.store.collection('done')) as Observable<Task[]>;
 
-  editTask(list: string, task: Task): void {}
+  constructor(private dialog: MatDialog, private store: AngularFirestore) {}
+
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true
+      }
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
+      if(result.delete) {
+        this.store.collection(list).doc(task.id).delete();
+      } else {
+        this.store.collection(list).doc(task.id).update(task);
+      }
+    })
+  }
 
   newTask() {
 	const dialogRef = this.dialog.open(TaskDialogComponent, {
-		width: '400px',
+		width: '270px',
 		data: {
 			task: {}
 		}
 	})
 	dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
-      if(result.task.title && result.task.description) {
-        this.todo.push(result.task)
-      }
+      if(!result.task.title && !result.task.description) return;
+      this.store.collection('todo').add(result.task);
     })
   }
 
@@ -45,9 +61,16 @@ export class KanbanComponent {
     if (event.previousContainer === event.container) {
       return;
     }
-    if (!event.container.data || !event.previousContainer.data) {
-      return;
-    }
+    
+    const item = event.previousContainer.data[event.previousIndex];
+    this.store.firestore.runTransaction(() => {
+      const promise = Promise.all([
+        this.store.collection(event.previousContainer.id).doc(item.id).delete(),
+        this.store.collection(event.container.id).add(item)
+      ]);
+      return promise;
+    })
+
     transferArrayItem(
       event.previousContainer.data,
       event.container.data,
